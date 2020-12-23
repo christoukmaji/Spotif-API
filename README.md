f0ff7861b0088454129f0b553802a6cc637132ff
christoukmaji <51040574+christoukmaji@users.noreply.github.com>
2020-12-23 13:12:02 -0800

    v1 of spotify album cover

---
+++ README.md
@@ -0,0 +1,16 @@
+# Scoreboard README  #
+## Chris Toukmaji | christoukmaji@gmail.com ##
+
+### Using Program
+This program is customized for a single panel LED matrix of size 64 x 32.
+
+### Features
+privacy
+
+### References
+
+
+
+### Issues
+
+### Future features
---
+++ led_matrix/spotify.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 02:54:08 2020
+
+@author: itsal
+"""
+import os, time, threading, random
+import feedparser
+import requests
+from PIL import Image, ImageFont, ImageDraw
+
+BITLY_ACCESS_TOKEN="BITLY_ACCESS_TOKEN"
+items=[]
+displayItems=[]
+
+
+
+def showOnLEDDisplay():
+    #for disp in displayItems[:60]:
+        #os.system("sudo ./examples-api-use/demo --led-cols=64 --led-rows=32 --led-chain=1 -t 60 -m 25 -D 1 "+disp)
+
+def run():
+    #response = requests.get(https://api.spotify.com/v1/me/player/currently-playing)
+    #print(response.status_code)
+if __name__ == '__main__':
+    run()
